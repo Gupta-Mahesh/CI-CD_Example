ccc341a288042c882adfae695da5f1bb78f80b6c
Mahesh Kumar Gupta <maheshkumargupta1792@gmail.com>
2024-10-23 23:03:17 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,18 @@
 import pytest
 from calculator import add, subtract, multiply, divide
 
+
 def test_add():
     assert add(3, 5) == 8
+
 
 def test_subtract():
     assert subtract(10, 5) == 5
 
+
 def test_multiply():
     assert multiply(4, 2) == 8
+
 
 def test_divide():
     assert divide(10, 2) == 5
